d593a169942e18718fb3ebf5eb8f70e7c921aab6
Robert Maru de Vera <maru@sheepd.com>
2022-10-27 14:26:46 +0200

    create skeleton views and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  def not_found; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'repositories#index'
+
+  resources :repositories, only: %i[index]
+
+  get '*all', to: 'application#not_found'
 end
